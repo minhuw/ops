9ffbe879256db848562cda1ee79c9a97519ff5d9
minhuw <wangmh15@gmail.com>
2024-10-25 03:08:40 +0000

    feat: add ddio-tune script

--- default.nix
+++ default.nix
@@ -3,4 +3,5 @@
   mlc = pkgs.callPackage ./pkgs/mlc.nix { };
   dpdk-pktgen = pkgs.callPackage ./pkgs/dpdk-pktgen.nix { };
   spectre-checker = pkgs.callPackage ./pkgs/spectre-checker.nix { };
+  ddio-tune = pkgs.callPackage ./pkgs/ddio-tune.nix { };
 }
