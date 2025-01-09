fb86378a9b063c5a7c3cbda801307ea3f819e803
minhuw <wangmh15@gmail.com>
2025-01-09 13:42:48 +0000

    feat: add l3-fwd package

--- default.nix
+++ default.nix
@@ -3,6 +3,7 @@
   mlc = pkgs.callPackage ./pkgs/mlc.nix { };
   dpdk-pktgen = pkgs.callPackage ./pkgs/dpdk-pktgen.nix { };
   dpdk-testpmd = pkgs.callPackage ./pkgs/dpdk.nix { };
+  dpdk-l3fwd = pkgs.callPackage ./pkgs/dpdk-l3fwd.nix { };
   spectre-checker = pkgs.callPackage ./pkgs/spectre-checker.nix { };
   ddio-tune = pkgs.callPackage ./pkgs/ddio-tune/ddio-tune.nix { };
   pqrs = pkgs.callPackage ./pkgs/pqrs.nix { };
---
+++ pkgs/dpdk-l3fwd.nix
@@ -0,0 +1,52 @@
+{ fetchFromGitHub, stdenv, libarchive, libbsd, libexecinfo, libelf, libbpf, ninja, openssl, python3, rdma-core, dtc, numactl, pkg-config, libpcap, jansson, mstflint, zlib }:
+stdenv.mkDerivation {
+  pname = "dpdk-l3fwd";
+  version = "24.03";
+
+  src = fetchFromGitHub {
+    owner = "dpdk";
+    repo = "dpdk";
+    rev = "v24.03";
+    sha256 = "sha256-BaGBi/EVSOR0abiMKhLUQIK/TZUYm4vLl9a5TGtSUvo=";
+  };
+
+  hardeningDisable = [ "fortify" ];
+
+  buildInputs = [
+    numactl
+    libarchive
+    libpcap
+    libbsd
+    libexecinfo
+    libelf
+    libbpf
+    jansson
+    openssl
+    dtc
+    mstflint
+    zlib
+  ];
+
+  propagatedBuildInputs = [
+    rdma-core
+  ];
+
+  nativeBuildInputs = [
+    ninja
+    pkg-config
+    (python3.withPackages (ps: with ps; [ pyelftools meson ]))
+  ];
+
+  configurePhase = ''
+    meson setup --prefix=$out -Denable_docs=false -Dtests=false -Dexamples="l3fwd" -Dbuildtype=release $(pwd)/build
+  '';
+
+  buildPhase = ''
+    meson compile -C $(pwd)/build
+  '';
+
+  installPhase = ''
+    meson install -C $(pwd)/build
+    cp $(pwd)/build/examples/dpdk-l3fwd $out/bin/
+  '';
+}
